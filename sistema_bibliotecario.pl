7f799419003794ea4c964e2400bb788b721395dc
Jorge-SDI <jorgesadi17@gmail.com>
2025-11-28 22:32:05 -0600

    Archivo del proyecto de logica

---
+++ sistema_bibliotecario.pl
@@ -0,0 +1,203 @@
+#definir conndiciones iniciales
+libro( lib1, 'El Quijote', 'Miguel de Cervantes', 'Aventura', 1605).
+libro( lib2, 'Cien Años de Soledad', 'Gabriel Garcia Marquez', 'Realismo Magico', 1967).
+libro( lib3, 'Cronica de una muerte anunciada', 'Gabriel Garcia Marquez', 'Narrativo', 1981).
+libro( lib4, 'La Sombra del Viento', 'Carlos Ruiz Zafon', 'Misterio', 2001).
+libro( lib5, 'Donde los arboles cantan', 'Laura Gallego', 'Fantasia', 2004).
+libro( lib6, 'El Principito', 'Antoine de Saint-Exupery', 'Infantil', 1943).
+libro( lib7, 'La Iliada', 'Homero', 'Epica', 750).
+libro( lib8, 'La Odisea', 'Homero', 'Epica', 725).
+libro( lib9, '1984', 'George Orwell', 'Ciencia Ficción', 1949).
+libro( lib10, 'Rebelion en la granja', 'George Orwell', 'Ciencia Ficción', 1945).
+libro( lib11, 'El libro salvaje', 'William Golding', 'Aventura', 1954).
+libro( lib12, 'Moby Dick', 'Herman Melville', 'Aventura', 1851).
+libro( lib13, 'Sherlock Holmes', 'Arthur Conan Doyle', 'Misterio', 1892).
+libro( lib14, 'El Gran Gatsby', 'F. Scott Fitzgerald', 'Narrativo', 1925).
+libro( lib15, 'To Kill a Mockingbird', 'Harper Lee', 'Narrativo', 1960).
+libro( lib16, 'El Señor de los Anillos', 'J.R.R. Tolkien', 'Fantasia', 1954).
+libro( lib17, 'Harry Potter y la piedra filosofal', 'J.K. Rowling', 'Fantasia', 1997).
+libro( lib18, 'El Hobbit', 'J.R.R. Tolkien', 'Fantasia', 1937).
+libro( lib19, 'Metodo Enigma', 'Rey Enigma', 'Didactico', 2022).
+libro( lib20, 'El Alquimista', 'Paulo Coelho', 'Ciencia Ficción', 1988).
+libro( lib21, 'El Código Da Vinci', 'Dan Brown', 'Misterio', 2003).
+libro( lib22, 'El Nombre de la Rosa', 'Umberto Eco', 'Misterio', 1980).
+libro( lib23, 'Los Otros Animatronicos', 'Kira Breed', 'Ciencia Ficcion', 2018).
+libro( lib24, 'Los Ojos de Plata', 'Kira Breed', 'Ciencia Ficcion', 2017).
+libro( lib25, 'El Juego de Ender', 'Orson Scott Card', 'Ciencia Ficcion', 1985).
+libro( lib26, 'Fundacion', 'Isaac Asimov', 'Ciencia Ficcion', 1951).
+libro( lib27, 'Neuromante', 'William Gibson', 'Ciencia Ficcion', 1984).
+libro( lib28, 'Snow Crash', 'Neal Stephenson', 'Ciencia Ficcion', 1992).
+libro( lib29, 'La Guia del Autoestopista Galactico', 'Douglas Adams', 'Ciencia Ficcion', 1979).
+libro( lib30, 'Dune', 'Frank Herbert', 'Ciencia Ficcion', 1965).
+libro( lib31, 'El Fin de la Eternidad', 'Isaac Asimov', 'Ciencia Ficcion', 1955).
+libro( lib32, 'Yo, Robot', 'Isaac Asimov', 'Ciencia Ficcion', 1950).
+libro( lib33, '2001: Una Odisea del Espacio', 'Arthur C. Clarke', 'Ciencia Ficcion', 1968).
+libro( lib34, 'Veinte Mil Leguas de Viaje Submarino', 'Julio Verne', 'Ciencia Ficcion', 1870).
+libro( lib35, 'La Maquina del Tiempo', 'H.G. Wells', 'Ciencia Ficcion', 1895).
+libro( lib36, 'Viaje al Centro de la Tierra', 'Julio Verne', 'Ciencia Ficcion', 1864).
+libro( lib37, 'El Mundo Perdido', 'Arthur Conan Doyle', 'Ciencia Ficcion', 1912).
+libro( lib38, 'Fahrenheit 451', 'Ray Bradbury', 'Ciencia Ficcion', 1953).
+libro( lib39, 'Criptonomicon', 'Neal Stephenson', 'Ciencia Ficcion', 1999).
+libro( lib40, 'El Hombre en el Castillo', 'Philip K. Dick', 'Ciencia Ficcion', 1962).
+libro( lib41, 'La vuelta al mundo en ochenta dias', 'Julio Verne', 'Ciencia Ficcion', 1873).
+libro( lib42, 'El fantasma de canterville', 'Oscar Wilde', 'Ciencia Ficcion', 1887).
+libro( lib43, 'El Cuarto Armario', 'Kira Breed', 'Ciencia Ficcion', 2020).
+libro( lib44, 'Nueva Historia Minima de Mexico', 'Colegio de Mexico', 'Historia', 2003).
+libro( lib45, 'Mi lucha', 'Adolf Hitler', 'Historia', 1925).
+libro( lib46, 'Historia de Mexico', 'Vicente Riva Palacio', 'Historia', 1870).
+libro( lib47, 'La Segunda Guerra Mundial', 'Winston Churchill', 'Historia', 1950).
+libro( lib48, 'El Holocausto', 'Deborah Lipstadt', 'Historia', 1993).
+libro( lib49, 'Breve Historia de la Revolucion Mexicana', 'Felipe Avila', 'Historia', 2016).
+libro( lib50, 'La Historia de la Independencia de Mexico', 'Jose Maria Morelos', 'Historia', 1810).
+libro( lib51, 'La Sucesion Presidencial', 'Francisco I. Madero', 'Historia', 1910).
+libro( lib52, 'Revolucion Mexicana', 'Venustiano Carranza', 'Historia', 1917).
+libro( lib53, 'El Porfiriato', 'Justo Sierra', 'Historia', 1900).
+libro( lib54, 'La Conquista de Mexico', 'Hernan Cortes', 'Historia', 1921).
+libro( lib55, 'La Caida del Imperio Azteca', 'Bernal Diaz del Castillo', 'Historia', 1962).
+libro( lib56, 'La Guerra Fria', 'John Lewis Gaddis', 'Historia', 2005).
+libro( lib57, 'La Revolucion Industrial', 'Arnold Toynbee', 'Historia', 1884).
+libro( lib58, 'La Edad Media', 'Jacques Le Goff', 'Historia', 1992).
+libro( lib59, 'El Renacimiento', 'Jacob Burckhardt', 'Historia', 1860).
+libro( lib60, 'La Ilustracion', 'Peter Gay', 'Historia', 1966).
+
+usuario(user1, 'Carlos Mendoza', 'Alumno').
+usuario(user2, 'Maria Lopez', 'Profesor').
+usuario(user3, 'Luis Garcia', 'Alumno').
+usuario(user4, 'Ana Torres', 'Profesor').
+usuario(user5, 'Jorge Sanchez', 'Alumno').
+usuario(user6, 'Sofia Ramirez', 'Externo').
+usuario(user7, 'Diego Fernandez', 'Alumno').
+usuario(user8, 'Elena Morales', 'Alumno').
+usuario(user9, 'Miguel Herrera', 'Profesor').
+usuario(user10, 'Laura Gutierrez', 'Externo').
+usuario(user11, 'Andres Jimenez', 'Alumno').
+usuario(user12, 'Natalia Ruiz', 'Profesor').
+usuario(user13, 'Aquiles Castro', 'Alumno').
+usuario(user14, 'Isabel Vega', 'Externo').
+usuario(user15, 'Fernando Rojas', 'Profesor').
+usuario(user16, 'Gabriela Silva', 'Alumno').
+usuario(user17, 'Ricardo Dominguez', 'Profesor').
+usuario(user18, 'Patricia Martinez', 'Externo').
+usuario(user19, 'Sergio Alvarez', 'Alumno').
+usuario(user20, 'Monica Castillo', 'Profesor').
+usuario(user21, 'Javier Navarro', 'Alumno').
+usuario(user22, 'Mario Flores', 'Externo').
+usuario(user23, 'Cecilia Ortega', 'Profesor').
+usuario(user24, 'Adrian Peña', 'Alumno').
+usuario(user25, 'Miguel Soto', 'Externo').
+usuario(user26, 'Veronica Rivas', 'Profesor').
+usuario(user27, 'Hector Cruz', 'Alumno').
+usuario(user28, 'Lorena Mendoza', 'Externo').
+usuario(user29, 'Pablo Jimenez', 'Profesor').
+usuario(user30, 'Daniela Romero', 'Alumno').
+usuario(user31, 'Estrella Guillen', 'Externo').
+usuario(user32, 'Elsi Dayli', 'Externo').
+usuario(user33, 'Luis Morales', 'Profesor').
+usuario(user34, 'Carmen Soto', 'Alumno').
+usuario(user35, 'Victor Hugo', 'Profesor').
+usuario(user36, 'Rosa Diaz', 'Externo').
+usuario(user37, 'Alfredo Pacheco', 'Alumno').
+usuario(user38, 'Claudia Rangel', 'Profesor').
+usuario(user39, 'Joaquin Salinas', 'Externo').
+usuario(user40, 'Natalia Campos', 'Alumno').
+usuario(user41, 'Emilio Vargas', 'Profesor').
+usuario(user42, 'Bianca Leon', 'Externo').
+usuario(user43, 'Felipe Cardenas', 'Alumno').
+usuario(user44, 'Adriana Fuentes', 'Profesor').
+usuario(user45, 'Santiago Mejia', 'Externo').
+usuario(user46, 'Valentina Rojas', 'Alumno').
+usuario(user47, 'Cesar Molina', 'Profesor').
+usuario(user48, 'Lorena Silva', 'Externo').
+usuario(user49, 'Javier Torres', 'Alumno').
+usuario(user50, 'Mariana Peña', 'Profesor').
+usuario(user51, 'Renata Hans', 'Externo').
+usuario(user52, 'Emilia Cruz', 'Alumno').
+usuario(user53, 'Fabian Ortega', 'Profesor').
+usuario(user54, 'Jordan Rios', 'Externo').
+usuario(user55, 'Ernesto Vargas', 'Alumno').
+usuario(user56, 'Javier Duarte', 'Profesor').
+usuario(user57, 'Camila Soto', 'Externo').
+usuario(user58, 'Brenda Morales', 'Alumno').
+usuario(user59, 'Santiago Rivas', 'Profesor').
+usuario(user60, 'Valeria Jimenez', 'Externo').
+usuario(user61, 'Esteban Castro', 'Alumno').
+usuario(user62, 'Daniela Moreno', 'Profesor').
+usuario(user63, 'Gabriel Torres', 'Externo').
+usuario(user64, 'Martin Zubimendia', 'Alumno').
+usuario(user65, 'Paula Jimenez', 'Profesor').
+usuario(user66, 'Mikel Arteta', 'Externo').
+usuario(user67, 'Diego Maradona', 'Alumno').
+usuario(user68, 'Lionel Messi', 'Profesor').
+usuario(user69, 'Cristiano Ronaldo', 'Externo').
+usuario(user70, 'Neymar Santos', 'Alumno').
+usuario(user71, 'Benito Rios', 'Profesor').
+usuario(user72, 'Marcel Ruiz', 'Externo').
+usuario(user73, 'Sergio Ramos', 'Alumno').
+usuario(user74, 'Jared Ortega', 'Profesor').
+usuario(user75, 'Francisco Rios', 'Externo').
+usuario(user76, 'Eden Hazard', 'Alumno').
+usuario(user77, 'Paul Pogba', 'Profesor').
+usuario(user78, 'Luis San Roman', 'Profesor').
+usuario(user79, 'Mario Kempes', 'Externo').
+usuario(user80, 'Omar Tapia', 'Alumno').
+usuario(user81, 'Memo Navarro', 'Profesor').
+usuario(user82, 'Erling Haaland', 'Externo').
+usuario(user83, 'Harry Kane', 'Alumno').
+usuario(user84, 'Cole Palmer', 'Profesor').
+usuario(user85, 'Johan Vazquez', 'Externo').
+usuario(user86, 'Pedro Medina', 'Alumno').
+usuario(user87, 'Rodrigo De Paul', 'Profesor').
+usuario(user88, 'Leandro Paredes', 'Externo').
+usuario(user89, 'Giovanni Cruz', 'Alumno').
+usuario(user90, 'Angel Rojas', 'Profesor').
+usuario(user91, 'Lautaro Martinez', 'Externo').
+usuario(user92, 'Paulo Dybala', 'Alumno').
+usuario(user93, 'Armando Gonzalez', 'Profesor').
+usuario(user94, 'German Martinez', 'Externo').
+usuario(user95, 'Carlos Tucan', 'Alumno').
+usuario(user96, 'Antonio Cortes', 'Profesor').
+usuario(user97, 'Renato Ismael', 'Externo').
+usuario(user98, 'Jorge Salazar', 'Alumno').
+usuario(user99, 'Erick Ibarra', 'Profesor').
+usuario(user100, 'Alexis Vega', 'Externo').
+
+prestamo(prest1, user1, lib1, '2025-01-10', '2025-01-24', 'Devuelto').
+prestamo(prest2, user2, lib5, '2025-02-15', '2025-03-01', 'Devuelto').
+prestamo(prest3, user3, lib10, '2025-03-05', '2025-03-19', 'Devuelto').
+prestamo(prest4, user4, lib15, '2025-04-12', '2025-04-26', 'Devuelto').
+prestamo(prest5, user5, lib20, '2025-05-20', '2025-06-03', 'Devuelto').
+prestamo(prest6, user6, lib25, '2025-06-01', ' ', 'Activo').
+prestamo(prest7, user7, lib30, '2025-06-10', ' ', 'Activo').
+prestamo(prest8, user8, lib35, '2025-06-15', ' ', 'Activo').
+prestamo(prest9, user9, lib40, '2025-06-18', ' ', 'Activo').
+prestamo(prest10, user10, lib45, '2025-06-20', '2025-07-04', 'Devuelto').
+prestamo(prest11, user11, lib2, '2025-01-22', '2025-02-05', 'Devuelto').
+prestamo(prest12, user12, lib7, '2025-02-28', '2025-03-13', 'Devuelto').
+prestamo(prest13, user13, lib12, '2025-03-18', '2025-04-01', 'Devuelto').
+prestamo(prest14, user14, lib17, '2025-04-25', '2025-05-09', 'Devuelto').
+prestamo(prest15, user15, lib22, '2025-05-30', ' ', 'Activo').
+prestamo(prest16, user16, lib27, '2025-06-05', ' ', 'Activo').
+prestamo(prest17, user17, lib32, '2025-06-12', ' ', 'Activo').
+prestamo(prest18, user18, lib37, '2025-06-19', ' ', 'Activo').
+prestamo(prest19, user19, lib42, '2025-06-21', '2025-07-05', 'Devuelto').
+prestamo(prest20, user20, lib47, '2025-01-15', '2025-01-29', 'Devuelto').
+prestamo(prest21, user21, lib3, '2025-02-10', ' ', 'Activo').
+prestamo(prest22, user22, lib8, '2025-03-12', '2025-03-26', 'Devuelto').
+prestamo(prest23, user23, lib13, '2025-04-08', '2025-04-22', 'Devuelto').
+prestamo(prest24, user24, lib18, '2025-05-18', '2025-06-01', 'Devuelto').
+prestamo(prest25, user25, lib23, '2025-06-02', ' ', 'Activo').
+prestamo(prest26, user26, lib28, '2025-06-11', ' ', 'Activo').
+prestamo(prest27, user27, lib33, '2025-06-16', ' ', 'Activo').
+prestamo(prest28, user28, lib38, '2025-06-20', ' ', 'Activo').
+prestamo(prest29, user29, lib43, '2025-06-22', '2025-07-06', 'Devuelto').
+prestamo(prest30, user30, lib48, '2025-01-18', '2025-02-01', 'Devuelto').
+prestamo(prest31, user31, lib4, '2025-02-14', '2025-02-28', 'Devuelto').
+prestamo(prest32, user32, lib9, '2025-03-20', ' ', 'Activo').
+prestamo(prest33, user33, lib14, '2025-04-22', '2025-05-06', 'Devuelto').
+prestamo(prest34, user34, lib19, '2025-05-25', '2025-06-08', 'Devuelto').
+prestamo(prest35, user35, lib24, '2025-06-03', ' ', 'Activo').
+prestamo(prest36, user36, lib29, '2025-06-13', ' ', 'Activo').
+prestamo(prest37, user37, lib34, '2025-06-17', ' ', 'Activo').
+prestamo(prest38, user38, lib39, '2025-06-21', ' ', 'Activo').
+prestamo(prest39, user39, lib44, '2025-06-23', '2025-07-07', 'Devuelto').
+prestamo(prest40, user40, lib49, '2025-01-20', '2025-02-03', 'Devuelto').
